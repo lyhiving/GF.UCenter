8a4983d750f18f9da33dd81d2c02fc595c22f025
lion <xiaolige945@vip.qq.com>
2016-03-10 01:16:59 +0800

    UCenter Client SDK For Unity3D

---
+++ UCenter.Unity3DSDK/Assets/Editor/EditorSample.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+public class EditorSample : EditorWindow
+{
+    //-------------------------------------------------------------------------
+    [MenuItem("UCenter/导出UCenterUnity3DSDK.unitypackage")]
+    static void exportGfJsonPackage()
+    {
+        string[] arr_assetpathname = new string[1];
+        arr_assetpathname[0] = "Assets/UCenterUnity3DSDK";
+        AssetDatabase.ExportPackage(arr_assetpathname, "UCenterUnity3DSDK.unitypackage", ExportPackageOptions.Recurse);
+
+        Debug.Log("Export UCenterUnity3DSDK.unitypackage Finished!");
+    }
+}
