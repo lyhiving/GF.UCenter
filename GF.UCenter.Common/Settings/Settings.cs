fc966f0c29f2cb57fdfcbb748eaf5e5d42601b76
lion <xiaolige945@vip.qq.com>
2016-03-21 03:30:50 +0800

    添加GF根名字空间，工程目录结构重命名。

---
+++ GF.UCenter.Common/Settings/Settings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UCenter.Common
+{
+    [Export]
+    public class Settings
+    {
+        [DefaultValue("ucenter")]
+        public string BucketName { get; set; }
+
+        [DefaultValue("ucenter-temp")]
+        public string TempBucketName { get; set; }
+
+        [DefaultValue("http://127.0.0.1:8091")]
+        public string ServerUris { get; set; }
+
+        [DefaultValue(1000)]
+        public uint DefaultOperationLifespan { get; set; }
+
+        [DefaultValue(true)]
+        public bool EnableTcpKeepAlives { get; set; }
+
+        [DefaultValue(1000 * 60 * 60)]
+        public uint TcpKeepAliveTime { get; set; }
+
+        [DefaultValue(1000)]
+        public uint TcpKeepAliveInterval { get; set; }
+
+        [DefaultValue(false)]
+        public bool UseSsl { get; set; }
+
+        [DefaultValue(10)]
+        public int PoolMaxSize { get; set; }
+
+        [DefaultValue(5)]
+        public int PoolMinSize { get; set; }
+
+        [DefaultValue(12000)]
+        public int PoolSendTimeout { get; set; }
+    }
+}
